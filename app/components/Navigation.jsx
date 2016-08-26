dd43fe16212f1b64eb1990e9accfc80a3de37fb0
Wahid Rahim <wahidrahim@gmail.com>
2016-08-26 11:13:19 -0400

    initial commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = (props) => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">React Timer App</li>
+          <li className="menu-text">
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+          </li>
+          <li className="menu-text">
+            <Link to="/">Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Created by <a href="https://github.com/wahidrahim">Wahid Rahim</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+module.exports = Navigation;
